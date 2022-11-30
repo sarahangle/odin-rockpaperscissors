dba772cfab4a6c7038a57fb99a848420aabfad42
sarahangle <sna44@cornell.edu>
2022-11-29 22:49:23 -0800

    Implement very basic UI

--- javascript.js
+++ javascript.js
@@ -14,45 +14,48 @@ function playRound(playerInput, computerChoice) {
         return `Tie! Both of you played ${playerInput}`;
     }
 
-    else if (playerInput === "Rock" && computerChoice === "Scissors") {return win(playerInput, computerChoice);}
-    else if (playerInput === "Paper" && computerChoice === "Rock") {return win(playerInput, computerChoice);}
-    else if (playerInput === "Scissors" && computerChoice === "Paper") {return win(playerInput, computerChoice);}
 
-    else if (playerInput === "Rock" && computerChoice === "Paper") {return lose(playerInput, computerChoice);}
-    else if (playerInput === "Paper" && computerChoice === "Scissors") {return lose(playerInput, computerChoice);}
-    else if (playerInput === "Scissors" && computerChoice === "Rock") {return lose(playerInput, computerChoice);}
+    else if (playerInput === "Rock" && computerChoice === "Scissors") {playerScore += 1;}
+    else if (playerInput === "Paper" && computerChoice === "Rock") {playerScore += 1;}
+    else if (playerInput === "Scissors" && computerChoice === "Paper") {playerScore += 1;}
 
-    else {return lose(null, null);}
+    else if (playerInput === "Rock" && computerChoice === "Paper") {computerScore += 1;}
+    else if (playerInput === "Paper" && computerChoice === "Scissors") {computerScore += 1;}
+    else if (playerInput === "Scissors" && computerChoice === "Rock") {computerScore += 1;}
+
 }
 
-function getPlayerInput() {
-    let playerInput = prompt("Rock, Paper, Scissors?");
-    playerInput = playerInput[0].toUpperCase() + playerInput.slice(1);
-
-    if (playerInput === "Rock" || playerInput === "Paper" || playerInput === "Scissors") {return playerInput;}
-    else {
-        console.log("I don't know what that was...")
-        return getPlayerInput();
-    }
-}
-
-function win(player, computer) {
-    playerScore++;
-    return `You win! ${player} beats ${computer}`;
-}
-
-function lose(player, computer) {
-    computerScore++;
-    return `You lost! ${computer} beats ${player}`;
-}
-
-function game() {
+function startGame() {
     playerScore = 0;
     computerScore = 0;
-    while (playerScore < 3 && computerScore < 3) {
-        let playerInput = getPlayerInput();
-        console.log(playRound(playerInput, getComputerChoice()));
-    }
-    if (playerScore === 3) {return `ULTIMATE WINNER. You beat the computer ${playerScore} to ${computerScore}`;}
-    else  {return `ULTIMATE LOSER. You lost the computer ${playerScore} to ${computerScore}`;}
+
+    const inputs = document.querySelectorAll('.input');
+    inputs.forEach(input => input.addEventListener('click', userInput));
 }
+
+function userInput(e) {
+    userSelection = this.dataset.key;
+    playRound(userSelection, getComputerChoice());
+    updateScore();
+}
+
+function updateScore() {
+    scoreDisplay = document.querySelector('.score');
+    scoreDisplay.textContent = `PLAYER: ${playerScore}  COMPUTER: ${computerScore}`;
+
+    if (playerScore >= 5) {endGame("YOU WIN!");}
+    else if (computerScore >= 5) {endGame("YOU LOSE")}
+}
+
+function endGame(message) {
+    scoreDisplay = document.querySelector('.score');
+    const node = document.createElement("div");
+    const nodeMessage = document.createTextNode(message);
+    node.appendChild(nodeMessage);
+    scoreDisplay.append(node);
+
+    const inputs = document.querySelectorAll('.input');
+    inputs.forEach(input => input.removeEventListener('click', userInput));
+}
+
+startGame()
